0c00768e36a83a3c10d3e748391df503c6b0450b
Daniel Soltys <danielsytlos@gmail.com>
2024-08-16 22:17:47 +0200

    Modified parametres of rsi calculation

--- trading_strategy_tester/indicators/rsi.py
+++ trading_strategy_tester/indicators/rsi.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
-def rsi(df: pd.DataFrame, column: str, length: int = 14) -> pd.Series:
-    delta = df[column].diff()
+def rsi(series: pd.Series, length: int = 14) -> pd.Series:
+    delta = series.diff()
     gain = (delta.where(delta > 0, 0)).rolling(window=length).mean()
     loss = (-delta.where(delta < 0, 0)).rolling(window=length).mean()
     rs = gain / loss
     return 100 - (100 / (1 + rs))
-
