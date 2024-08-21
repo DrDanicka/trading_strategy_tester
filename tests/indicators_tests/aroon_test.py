a947cb35e363727e54c6f99b6f6f110afbd5babe
Daniel Soltys <danielsytlos@gmail.com>
2024-08-21 12:04:49 +0200

    Changed name of column in aroon tests

--- tests/indicators_tests/aroon_test.py
+++ tests/indicators_tests/aroon_test.py
@@ -86,7 +86,7 @@ class TestAroon(unittest.TestCase):
             57.14, 50.00, 100.00, 100.00, 100.00, 100.00, 92.86, 85.71, 100.00, 100.00,
             92.86, 85.71, 78.57, 71.43, 64.29, 57.14, 50.00, 42.86, 35.71, 28.57
         ], name=f'{ticker}_AROONUP_14').reset_index(drop=True)
-        aroon_up_series = AroonUp(ticker, 'High', 14)
+        aroon_up_series = AroonUp(ticker, 14)
         calculated_aroon_up = aroon_up_series.get_data(self.downloader).tail(20).reset_index(drop=True).round(2)
         pd.testing.assert_series_equal(calculated_aroon_up, trading_view_aroon_up)
 
@@ -102,7 +102,7 @@ class TestAroon(unittest.TestCase):
             0.00, 0.00, 0.00, 85.71, 78.57, 71.43, 64.29, 57.14, 50.00, 42.86, 35.71,
             28.57, 21.43, 14.29, 7.14, 0.00, 0.00, 100.00, 92.86, 85.71
         ], name=f'{ticker}_AROONDOWN_14').reset_index(drop=True)
-        aroon_down_series = AroonDown(ticker, 'Low', 14)
+        aroon_down_series = AroonDown(ticker, 14)
         calculated_aroon_down = aroon_down_series.get_data(self.downloader).tail(20).reset_index(drop=True).round(2)
         pd.testing.assert_series_equal(calculated_aroon_down, trading_view_aroon_down)
 
