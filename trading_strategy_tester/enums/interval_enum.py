68edfed920016ec4b3fa86bcee02ab57acb48c9e
Daniel Soltys <danielsytlos@gmail.com>
2024-08-27 21:28:12 +0200

    Added documentary comments to enums

--- trading_strategy_tester/enums/interval_enum.py
+++ trading_strategy_tester/enums/interval_enum.py
@@ -1,16 +1,51 @@
 from enum import Enum
 
+
 class Interval(Enum):
-    ONE_MINUTE = '1m'
-    TWO_MINUTES = '2m'
-    FIVE_MINUTES = '5m'
-    FIFTEEN_MINUTES = '15m'
-    THIRTY_MINUTES = '30m'
-    SIXTY_MINUTES = '60m'
-    NINETY_MINUTES = '90m'
-    ONE_HOUR = '1h'
-    ONE_DAY = '1d'
-    FIVE_DAYS = '5d'
-    ONE_WEEK = '1wk'
-    ONE_MONTH = '1mo'
-    THREE_MONTHS = '3mo'
+    """
+    Interval is an enumeration that represents various time intervals commonly used in data analysis,
+    particularly in the context of financial data or time series data.
+
+    Attributes:
+    ----------
+    ONE_MINUTE : str
+        Represents a 1-minute interval ('1m').
+    TWO_MINUTES : str
+        Represents a 2-minute interval ('2m').
+    FIVE_MINUTES : str
+        Represents a 5-minute interval ('5m').
+    FIFTEEN_MINUTES : str
+        Represents a 15-minute interval ('15m').
+    THIRTY_MINUTES : str
+        Represents a 30-minute interval ('30m').
+    SIXTY_MINUTES : str
+        Represents a 60-minute interval ('60m').
+    NINETY_MINUTES : str
+        Represents a 90-minute interval ('90m').
+    ONE_HOUR : str
+        Represents a 1-hour interval ('1h').
+    ONE_DAY : str
+        Represents a 1-day interval ('1d').
+    FIVE_DAYS : str
+        Represents a 5-day interval ('5d').
+    ONE_WEEK : str
+        Represents a 1-week interval ('1wk').
+    ONE_MONTH : str
+        Represents a 1-month interval ('1mo').
+    THREE_MONTHS : str
+        Represents a 3-month interval ('3mo').
+    """
+
+    ONE_MINUTE = '1m'  # 1-minute interval
+    TWO_MINUTES = '2m'  # 2-minute interval
+    FIVE_MINUTES = '5m'  # 5-minute interval
+    FIFTEEN_MINUTES = '15m'  # 15-minute interval
+    THIRTY_MINUTES = '30m'  # 30-minute interval
+    SIXTY_MINUTES = '60m'  # 60-minute interval
+    NINETY_MINUTES = '90m'  # 90-minute interval
+    ONE_HOUR = '1h'  # 1-hour interval
+    ONE_DAY = '1d'  # 1-day interval
+    FIVE_DAYS = '5d'  # 5-day interval
+    ONE_WEEK = '1wk'  # 1-week interval
+    ONE_MONTH = '1mo'  # 1-month interval
+    THREE_MONTHS = '3mo'  # 3-month interval
--- trading_strategy_tester/enums/smoothing_enum.py
+++ trading_strategy_tester/enums/smoothing_enum.py
@@ -1,7 +1,23 @@
 from enum import Enum
 
 class SmoothingType(Enum):
-    RMA = 'RMA'
-    SMA = 'SMA'
-    EMA = 'EMA'
-    WMA = 'WMA'
+    """
+    SmoothingType is an enumeration that represents different types of smoothing techniques used
+    in data analysis, particularly in the context of time series and financial data.
+
+    Attributes:
+    ----------
+    RMA : str
+        Represents the Running Moving Average (RMA) smoothing technique.
+    SMA : str
+        Represents the Simple Moving Average (SMA) smoothing technique.
+    EMA : str
+        Represents the Exponential Moving Average (EMA) smoothing technique.
+    WMA : str
+        Represents the Weighted Moving Average (WMA) smoothing technique.
+    """
+
+    RMA = 'RMA'  # Running Moving Average
+    SMA = 'SMA'  # Simple Moving Average
+    EMA = 'EMA'  # Exponential Moving Average
+    WMA = 'WMA'  # Weighted Moving Average
