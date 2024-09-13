c5cfea115df680df98d47d8a8c534133cb42f37a
Daniel Soltys <danielsytlos@gmail.com>
2024-09-13 10:24:31 +0200

    Added new tests to long_short combination

--- tests/decision_columns_tests/long_short_combination_cleaning_data.py
+++ tests/decision_columns_tests/long_short_combination_cleaning_data.py
@@ -34,5 +34,129 @@ class TestCleaningDataLongShortCombination(unittest.TestCase):
         pd.testing.assert_frame_equal(df, df_expected)
 
 
+    def test_buy_sell_at_the_same_day_in_the_beginning(self):
+        # Arrange
+        df = pd.DataFrame(
+            {
+                'BUY': [True, False, False, False, False],
+                'SELL': [True, False, True, False, False]
+            }
+        )
+
+        df_expected = pd.DataFrame(
+            {
+                'BUY': [False, False, False, False, True],
+                'SELL': [False, False, True, False, False],
+                'Long': [None, None, None, None, None],
+                'Short': [None, None, 'ShortEntry', None, 'ShortExit']
+            }
+        )
+
+        # Act
+        self.long_short_position.clean_buy_sell_columns(df)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_buy_sell_at_the_same_day_when_bought(self):
+        # Arrange
+        df = pd.DataFrame(
+            {
+                'BUY': [True, True, False, False, False],
+                'SELL': [False, True, True, False, False]
+            }
+        )
+
+        df_expected = pd.DataFrame(
+            {
+                'BUY': [True, False, False, False, True],
+                'SELL': [False, False, True, False, False],
+                'Long': ['LongEntry', None, 'LongExit', None, None],
+                'Short': [None, None, 'ShortEntry', None, 'ShortExit']
+            }
+        )
+
+        # Act
+        self.long_short_position.clean_buy_sell_columns(df)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_buy_sell_at_the_same_day_when_sold(self):
+        # Arrange
+        df = pd.DataFrame(
+            {
+                'BUY': [True, False, False, True, False],
+                'SELL': [False, False, True, True, False]
+            }
+        )
+
+        df_expected = pd.DataFrame(
+            {
+                'BUY': [True, False, False, False, True],
+                'SELL': [False, False, True, False, False],
+                'Long': ['LongEntry', None, 'LongExit', None, None],
+                'Short': [None, None, 'ShortEntry', None, 'ShortExit']
+            }
+        )
+
+        # Act
+        self.long_short_position.clean_buy_sell_columns(df)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_buy_sell_at_the_same_day_when_sold_in_the_end(self):
+        # Arrange
+        df = pd.DataFrame(
+            {
+                'BUY': [True, False, False, False, True],
+                'SELL': [False, False, True, False, True]
+            }
+        )
+
+        df_expected = pd.DataFrame(
+            {
+                'BUY': [True, False, False, False, True],
+                'SELL': [False, False, True, False, False],
+                'Long': ['LongEntry', None, 'LongExit', None, None],
+                'Short': [None, None, 'ShortEntry', None, 'ShortExit']
+            }
+        )
+
+        # Act
+        self.long_short_position.clean_buy_sell_columns(df)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_buy_sell_at_the_same_day_when_bought_in_the_end(self):
+        # Arrange
+        df = pd.DataFrame(
+            {
+                'BUY': [True, False, False, False, True],
+                'SELL': [False, False, False, False, True]
+            }
+        )
+
+        df_expected = pd.DataFrame(
+            {
+                'BUY': [True, False, False, False, False],
+                'SELL': [False, False, False, False, True],
+                'Long': ['LongEntry', None, None, None, 'LongExit'],
+                'Short': [None, None, None, None, None]
+            }
+        )
+
+        # Act
+        self.long_short_position.clean_buy_sell_columns(df)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
 if __name__ == '__main__':
     unittest.main()
