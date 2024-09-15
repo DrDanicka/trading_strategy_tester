a3e9f8f943f06723be40fe9abae410f4ae34b4bd
Daniel Soltys <danielsytlos@gmail.com>
2024-09-15 23:49:53 +0200

    Moved EOM to volume

--- tests/indicators_tests/eom_test.py
+++ tests/indicators_tests/eom_test.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 from trading_strategy_tester.download.download_module import DownloadModule
 from trading_strategy_tester.enums.source_enum import SourceType
-from trading_strategy_tester.indicators.eom import eom
+from trading_strategy_tester.indicators.volume.eom import eom
 from trading_strategy_tester.trading_series.eom_series import EOM
 
 class TestEOM(unittest.TestCase):
