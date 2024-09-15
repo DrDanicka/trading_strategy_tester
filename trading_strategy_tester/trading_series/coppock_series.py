5816a44931913bcc1034d6780b0d2e1e136e0ee4
Daniel Soltys <danielsytlos@gmail.com>
2024-09-15 23:32:31 +0200

    Moved coppock indicator to momentum

--- trading_strategy_tester/trading_series/coppock_series.py
+++ trading_strategy_tester/trading_series/coppock_series.py
@@ -3,7 +3,7 @@ import pandas as pd
 from trading_strategy_tester.download.download_module import DownloadModule
 from trading_strategy_tester.enums.source_enum import SourceType
 from trading_strategy_tester.trading_series.trading_series import TradingSeries
-from trading_strategy_tester.indicators.cop import cop
+from trading_strategy_tester.indicators.momentum.cop import cop
 
 class COPPOCK(TradingSeries):
     """
