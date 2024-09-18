3e1308ba5254f57b91ae6b7182bf3ead25fd8aa7
Daniel Soltys <danielsytlos@gmail.com>
2024-09-18 07:57:32 +0200

    Moved trend conditions to own dir

--- tests/conditions_tests/uptrend_condition_test.py
+++ tests/conditions_tests/uptrend_condition_test.py
@@ -3,7 +3,7 @@ import unittest
 
 import pandas as pd
 
-from trading_strategy_tester.conditions.uptrend_for_x_days import UptrendForXDaysCondition
+from trading_strategy_tester.conditions.trend_conditions.uptrend_for_x_days import UptrendForXDaysCondition
 from trading_strategy_tester.download.download_module import DownloadModule
 from trading_strategy_tester.trading_series.testing_series import TestingSeries
 
