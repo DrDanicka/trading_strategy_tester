df0d2a5b2de59d55c907447199156eb06eff036b
Daniel Soltys <danielsytlos@gmail.com>
2024-09-18 11:22:34 +0200

    Added documentary comments to trend conditions

--- trading_strategy_tester/conditions/trend_conditions/uptrend_for_x_days_condition.py
+++ trading_strategy_tester/conditions/trend_conditions/uptrend_for_x_days_condition.py
@@ -6,21 +6,44 @@ from trading_strategy_tester.trading_plot.trading_plot import TradingPlot
 from trading_strategy_tester.trading_plot.uptrend_plot import UptrendPlot
 from trading_strategy_tester.trading_series.trading_series import TradingSeries
 
-
 class UptrendForXDaysCondition(Condition):
     def __init__(self, series: TradingSeries, number_of_days: int):
+        """
+        Initialize the UptrendForXDaysCondition with the given series and number of days.
+
+        :param series: The TradingSeries object containing the data series to evaluate.
+        :type series: TradingSeries
+        :param number_of_days: The number of days over which the uptrend should be evaluated.
+        :type number_of_days: int
+        """
         self.series = series
         self.number_of_days = number_of_days
 
     def evaluate(self, downloader: DownloadModule, df: pd.DataFrame) -> (pd.Series, pd.Series):
-        series : pd.Series = self.series.get_data(downloader, df)
+        """
+        Evaluate the condition over the given dataframe and return the result series and signal series.
 
+        The condition checks whether the series has been in an uptrend for the specified number of days.
+
+        :param downloader: The DownloadModule used to fetch the data.
+        :type downloader: DownloadModule
+        :param df: The dataframe containing the data to evaluate.
+        :type df: pd.DataFrame
+        :return: A tuple containing:
+            - is_uptrend: A pandas Series where True indicates that the series has been in an uptrend for the specified number of days.
+            - signal_series: A pandas Series with descriptive strings where the condition is True, otherwise None.
+        :rtype: tuple(pd.Series, pd.Series)
+        """
+        series: pd.Series = self.series.get_data(downloader, df)
+
+        # Apply rolling window to check for uptrend
         is_uptrend = series.rolling(window=self.number_of_days).apply(
             lambda x: (x.diff().fillna(0) >= 0).all(), raw=False
         )
         is_uptrend = is_uptrend.fillna(0).astype(bool)
         is_uptrend.name = None
 
+        # Generate signal series with descriptive strings
         signal_series = is_uptrend.apply(
             lambda x: f'UptrendForXDaysSignal({self.number_of_days}, {self.series.get_name()})' if x else None
         )
@@ -28,10 +51,26 @@ class UptrendForXDaysCondition(Condition):
         return is_uptrend, signal_series
 
     def get_graphs(self, downloader: DownloadModule, df: pd.DataFrame) -> [TradingPlot]:
+        """
+        Generate the plot for the condition.
+
+        :param downloader: The DownloadModule used to fetch the data.
+        :type downloader: DownloadModule
+        :param df: The dataframe containing the data to plot.
+        :type df: pd.DataFrame
+        :return: A list containing the UptrendPlot.
+        :rtype: list[TradingPlot]
+        """
         return [UptrendPlot(
             self.series.get_data(downloader, df),
             self.number_of_days
         )]
 
     def to_string(self) -> str:
+        """
+        Provide a string representation of the condition for debugging or logging.
+
+        :return: A string representation of the UptrendForXDaysCondition.
+        :rtype: str
+        """
         return f'UptrendForXDaysCondition({self.number_of_days}, {self.series.get_name()})'
