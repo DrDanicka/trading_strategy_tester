12c713c44e36e1925e81c12be251fd1361107969
Daniel Soltys <danielsytlos@gmail.com>
2024-09-18 21:12:23 +0200

    Added functionality to take profit in all position types with tests

--- tests/decision_columns_tests/take_profit_test.py
+++ tests/decision_columns_tests/take_profit_test.py
@@ -1,115 +1,493 @@
 import unittest
 import pandas as pd
 from trading_strategy_tester.conditions.stoploss_takeprofit.take_profit import TakeProfit
+from trading_strategy_tester.enums.position_type_enum import PositionTypeEnum
+
 
 class TestTakeProfit(unittest.TestCase):
 
     def setUp(self):
         pass
 
-    def test_take_profit_5_percent(self):
+    def test_long_take_profit_5_percent(self):
         # Arrange
-        take_profit = TakeProfit(5)
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.LONG
+        expected_signal = f'TakeProfit({percent})'
 
         df = pd.DataFrame({
             'Close': [50, 54, 55, 47, 51],
             'BUY': [True, False, False, False, False],
             'SELL': [False, False, False, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
         })
 
         df_expected = pd.DataFrame({
             'Close': [50, 54, 55, 47, 51],
             'BUY': [True, False, False, False, False],
             'SELL': [False, True, False, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, expected_signal, None, None, None]
         })
 
         # Act
-        take_profit.set_take_profit(df)
+        take_profit.set_take_profit(df, position_type)
 
         # Assert
         pd.testing.assert_frame_equal(df, df_expected)
 
-    def test_take_profit_5_percent_with_no_take_profit(self):
+
+    def test_long_take_profit_5_percent_with_no_take_profit(self):
         # Arrange
-        take_profit = TakeProfit(5)
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.LONG
+        expected_signal = f'TakeProfit({percent})'
 
         df = pd.DataFrame({
             'Close': [50, 52, 49, 47, 51],
             'BUY': [True, False, False, False, False],
             'SELL': [False, False, False, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
         })
 
         df_expected = pd.DataFrame({
             'Close': [50, 52, 49, 47, 51],
             'BUY': [True, False, False, False, False],
             'SELL': [False, False, False, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
         })
 
         # Act
-        take_profit.set_take_profit(df)
+        take_profit.set_take_profit(df, position_type)
 
         # Assert
         pd.testing.assert_frame_equal(df, df_expected)
 
-    def test_take_profit_5_percent_sell_on_take_profit(self):
+
+    def test_long_take_profit_5_percent_sell_on_take_profit(self):
         # Arrange
-        take_profit = TakeProfit(5)
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.LONG
+        expected_signal = f'TakeProfit({percent})'
 
         df = pd.DataFrame({
             'Close': [50, 52, 55, 47, 51],
             'BUY': [True, False, False, False, False],
             'SELL': [False, False, True, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
         })
 
         df_expected = pd.DataFrame({
             'Close': [50, 52, 55, 47, 51],
             'BUY': [True, False, False, False, False],
             'SELL': [False, False, True, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
         })
 
         # Act
-        take_profit.set_take_profit(df)
+        take_profit.set_take_profit(df, position_type)
 
         # Assert
         pd.testing.assert_frame_equal(df, df_expected)
 
-    def test_take_profit_5_percent_buy_on_take_profit(self):
+
+    def test_long_take_profit_5_percent_buy_on_take_profit(self):
         # Arrange
-        take_profit = TakeProfit(5)
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.LONG
+        expected_signal = f'TakeProfit({percent})'
 
         df = pd.DataFrame({
             'Close': [50, 52, 55, 47, 51],
             'BUY': [True, False, True, False, False],
             'SELL': [False, False, False, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
         })
 
         df_expected = pd.DataFrame({
             'Close': [50, 52, 55, 47, 51],
-            'BUY': [True, False, True, False, False],
+            'BUY': [True, False, False, False, False],
             'SELL': [False, False, True, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, expected_signal, None, None]
         })
 
         # Act
-        take_profit.set_take_profit(df)
+        take_profit.set_take_profit(df, position_type)
 
         # Assert
         pd.testing.assert_frame_equal(df, df_expected)
 
-    def test_take_profit_5_percent_multiple_trades(self):
+
+    def test_long_take_profit_5_percent_multiple_trades(self):
         # Arrange
-        take_profit = TakeProfit(5)
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.LONG
+        expected_signal = f'TakeProfit({percent})'
 
         df = pd.DataFrame({
             'Close': [50, 52, 55, 60, 51],
             'BUY': [True, False, True, False, False],
             'SELL': [False, True, False, False, True],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
         })
 
         df_expected = pd.DataFrame({
             'Close': [50, 52, 55, 60, 51],
             'BUY': [True, False, True, False, False],
             'SELL': [False, True, False, True, True],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, expected_signal, None],
         })
+
+        # Act
+        take_profit.set_take_profit(df, position_type)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_short_take_profit_5_percent(self):
+        # Arrange
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.SHORT
+        expected_signal = f'TakeProfit({percent})'
+
+        df = pd.DataFrame({
+            'Close': [50, 48, 48, 45, 51],
+            'BUY': [False, False, False, False, False],
+            'SELL': [True, False, False, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        df_expected = pd.DataFrame({
+            'Close': [50, 48, 48, 45, 51],
+            'BUY': [False, False, False, True, False],
+            'SELL': [True, False, False, False, False],
+            'BUY_Signals': [None, None, None, expected_signal, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        # Act
+        take_profit.set_take_profit(df, position_type)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_short_take_profit_5_percent_with_no_take_profit(self):
+        # Arrange
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.SHORT
+        expected_signal = f'TakeProfit({percent})'
+
+        df = pd.DataFrame({
+            'Close': [50, 48, 48, 49, 51],
+            'BUY': [False, False, False, False, False],
+            'SELL': [True, False, False, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        df_expected = pd.DataFrame({
+            'Close': [50, 48, 48, 49, 51],
+            'BUY': [False, False, False, False, False],
+            'SELL': [True, False, False, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        # Act
+        take_profit.set_take_profit(df, position_type)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_short_take_profit_5_percent_sell_on_take_profit(self):
+        # Arrange
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.SHORT
+        expected_signal = f'TakeProfit({percent})'
+
+        df = pd.DataFrame({
+            'Close': [50, 48, 47, 49, 51],
+            'BUY': [False, False, False, False, False],
+            'SELL': [True, False, True, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        df_expected = pd.DataFrame({
+            'Close': [50, 48, 47, 49, 51],
+            'BUY': [False, False, True, False, False],
+            'SELL': [True, False, False, False, False],
+            'BUY_Signals': [None, None, expected_signal, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        # Act
+        take_profit.set_take_profit(df, position_type)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_short_take_profit_5_percent_buy_on_take_profit(self):
+        # Arrange
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.SHORT
+        expected_signal = f'TakeProfit({percent})'
+
+        df = pd.DataFrame({
+            'Close': [50, 48, 47, 49, 51],
+            'BUY': [False, False, True, False, False],
+            'SELL': [True, False, False, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        df_expected = pd.DataFrame({
+            'Close': [50, 48, 47, 49, 51],
+            'BUY': [False, False, True, False, False],
+            'SELL': [True, False, False, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        # Act
+        take_profit.set_take_profit(df, position_type)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_short_take_profit_5_percent_multiple_trades(self):
+        # Arrange
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.SHORT
+        expected_signal = f'TakeProfit({percent})'
+
+        df = pd.DataFrame({
+            'Close': [50, 48, 47, 49, 44],
+            'BUY': [False, False, False, False, True],
+            'SELL': [True, True, False, True, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        df_expected = pd.DataFrame({
+            'Close': [50, 48, 47, 49, 44],
+            'BUY': [False, False, True, False, True],
+            'SELL': [True, True, False, True, False],
+            'BUY_Signals': [None, None, expected_signal, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        # Act
+        take_profit.set_take_profit(df, position_type)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_long_short_combination_take_profit_5_percent_one_buy(self):
+        # Arrange
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.LONG_SHORT_COMBINED
+        expected_signal = f'TakeProfit({percent})'
+
+        df = pd.DataFrame({
+            'Close': [50, 52, 53, 49, 44],
+            'BUY': [True, False, False, False, False],
+            'SELL': [False, False, False, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        df_expected = pd.DataFrame({
+            'Close': [50, 52, 53, 49, 44],
+            'BUY': [True, False, False, False, False],
+            'SELL': [False, False, True, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, expected_signal, None, None]
+        })
+
+        # Act
+        take_profit.set_take_profit(df, position_type)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_long_short_combination_take_profit_5_percent_one_sell(self):
+        # Arrange
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.LONG_SHORT_COMBINED
+        expected_signal = f'TakeProfit({percent})'
+
+        df = pd.DataFrame({
+            'Close': [50, 52, 47, 49, 44],
+            'BUY': [False, False, False, False, False],
+            'SELL': [True, False, False, False, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        df_expected = pd.DataFrame({
+            'Close': [50, 52, 47, 49, 44],
+            'BUY': [False, False, True, False, False],
+            'SELL': [True, False, False, False, False],
+            'BUY_Signals': [None, None, expected_signal, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        # Act
+        take_profit.set_take_profit(df, position_type)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_long_short_combination_take_profit_5_percent_buy_sell_combination(self):
+        # Arrange
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.LONG_SHORT_COMBINED
+        expected_signal = f'TakeProfit({percent})'
+
+        df = pd.DataFrame({
+            'Close': [50, 52, 53, 49, 44],
+            'BUY': [True, False, True, False, False],
+            'SELL': [False, False, False, True, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        df_expected = pd.DataFrame({
+            'Close': [50, 52, 53, 49, 44],
+            'BUY': [True, False, False, False, True],
+            'SELL': [False, False, True, True, False],
+            'BUY_Signals': [None, None, None, None, expected_signal],
+            'SELL_Signals': [None, None, expected_signal, None, None]
+        })
+
+        # Act
+        take_profit.set_take_profit(df, position_type)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_long_short_combination_take_profit_5_percent_sell_at_take_profit(self):
+        # Arrange
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.LONG_SHORT_COMBINED
+        expected_signal = f'TakeProfit({percent})'
+
+        df = pd.DataFrame({
+            'Close': [50, 52, 53, 49, 44],
+            'BUY': [True, False, False, False, False],
+            'SELL': [False, False, True, True, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        df_expected = pd.DataFrame({
+            'Close': [50, 52, 53, 49, 44],
+            'BUY': [True, False, False, True, False],
+            'SELL': [False, False, True, False, False],
+            'BUY_Signals': [None, None, None, expected_signal, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        # Act
+        take_profit.set_take_profit(df, position_type)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_long_short_combination_take_profit_5_percent_buy_sell_at_take_profit(self):
+        # Arrange
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.LONG_SHORT_COMBINED
+        expected_signal = f'TakeProfit({percent})'
+
+        df = pd.DataFrame({
+            'Close': [50, 52, 53, 49, 44],
+            'BUY': [True, False, True, False, False],
+            'SELL': [False, False, True, True, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        df_expected = pd.DataFrame({
+            'Close': [50, 52, 53, 49, 44],
+            'BUY': [True, False, False, False, True],
+            'SELL': [False, False, True, True, False],
+            'BUY_Signals': [None, None, None, None, expected_signal],
+            'SELL_Signals': [None, None, expected_signal, None, None]
+        })
+
+        # Act
+        take_profit.set_take_profit(df, position_type)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
+
+    def test_long_short_combination_take_profit_5_percent_false_buy_sell_at_take_profit(self):
+        # Arrange
+        percent = 5
+        take_profit = TakeProfit(percent)
+        position_type = PositionTypeEnum.LONG_SHORT_COMBINED
+        expected_signal = f'TakeProfit({percent})'
+
+        df = pd.DataFrame({
+            'Close': [50, 52, 53, 49, 44],
+            'BUY': [True, False, False, False, False],
+            'SELL': [False, False, False, True, False],
+            'BUY_Signals': [None, None, None, None, None],
+            'SELL_Signals': [None, None, None, None, None]
+        })
+
+        df_expected = pd.DataFrame({
+            'Close': [50, 52, 53, 49, 44],
+            'BUY': [True, False, False, False, True],
+            'SELL': [False, False, True, True, False],
+            'BUY_Signals': [None, None, None, None, expected_signal],
+            'SELL_Signals': [None, None, expected_signal, None, None]
+        })
+
+        # Act
+        take_profit.set_take_profit(df, position_type)
+
+        # Assert
+        pd.testing.assert_frame_equal(df, df_expected)
+
 
 if __name__ == '__main__':
     unittest.main()
--- trading_strategy_tester/conditions/stoploss_takeprofit/take_profit.py
+++ trading_strategy_tester/conditions/stoploss_takeprofit/take_profit.py
@@ -1,61 +1,107 @@
 import pandas as pd
 
+from trading_strategy_tester.enums.position_type_enum import PositionTypeEnum
+from trading_strategy_tester.enums.source_enum import SourceType
+
+
 class TakeProfit:
     """
-    A class to apply a take-profit strategy to trading data.
+    Implements a take-profit strategy for trading data.
 
-    The TakeProfit class allows for the implementation of a take-profit strategy based on a specified percentage.
-    It monitors the price increase from the buying price and sets a 'SELL' signal when the price exceeds the take-profit threshold.
+    This class monitors the price increase from the buying price or decrease from the selling price
+    and applies a 'SELL' or 'BUY' signal when the price exceeds the specified take-profit percentage threshold.
     """
 
     def __init__(self, percentage: float):
         """
-        Initializes the take-profit strategy with the specified percentage.
+        Initializes the TakeProfit class with the take-profit percentage.
 
-        Parameters:
-        -----------
-        percentage : float
-            The percentage used to determine the take-profit threshold.
-            For example, if set to 10, a 'SELL' signal will be triggered when the price increases by 10% from the buying price.
+        :param percentage: The percentage threshold for the take-profit. For instance,
+                           if set to 10, a 'SELL' signal is triggered when the price increases
+                           by 10% from the buying price, or a 'BUY' signal is triggered
+                           when the price decreases by 10% from the selling price.
+        :type percentage: float
         """
         self.percentage = percentage
 
-    def set_take_profit(self, df: pd.DataFrame):
+    def set_take_profit(self, df: pd.DataFrame, position_type: PositionTypeEnum):
         """
-        Adjusts the 'SELL' signals in the DataFrame based on the take-profit strategy.
+        Applies the take-profit strategy by adjusting 'SELL' and 'BUY' signals based on the current price.
 
-        This method iterates over the DataFrame and sets 'SELL' signals when the current price
-        rises to or above a calculated take-profit threshold. The threshold is determined as a 
-        percentage increase from the buying price.
+        The function iterates over the DataFrame, and when the current price rises (or falls) to or beyond
+        the calculated take-profit threshold, it sets 'SELL' or 'BUY' signals accordingly.
 
-        Parameters:
-        -----------
-        df : pd.DataFrame
-            A DataFrame containing columns 'Close', 'BUY', and 'SELL'.
-            'Close' represents the closing price of the asset.
-            'BUY' indicates where buying actions occurred.
-            'SELL' will be modified by this function to indicate where take-profit sell actions should occur.
+        :param df: A DataFrame containing columns 'Close', 'BUY', and 'SELL'.
+                   - 'Close' represents the closing price of the asset.
+                   - 'BUY' indicates where buy actions occurred.
+                   - 'SELL' will be modified to indicate where take-profit sell actions should occur.
+        :type df: pd.DataFrame
 
-        Returns:
-        --------
-        None
-            The DataFrame is modified in place.
+        :param position_type: The type of position being used (LONG, SHORT, or LONG_SHORT_COMBINED).
+                              This determines how the take-profit logic is applied (whether to monitor for long, short, or both types of trades).
+        :type position_type: PositionTypeEnum
+
+        :return: None
+        :rtype: None
         """
 
+        # Initialize tracking variables for buy/sell state
         bought = False
+        sold = False
         buying_price = 0
-        # This represents how much can the trade go down until it hits percentage stop-loos
-        value_threshold = 0
+        selling_price = 0
+        value_threshold = 0  # How much the trade can rise or fall before reaching the take-profit
+
+        # Determine whether the strategy supports long, short, or both
+        has_long = position_type == PositionTypeEnum.LONG or position_type == PositionTypeEnum.LONG_SHORT_COMBINED
+        has_short = position_type == PositionTypeEnum.SHORT or position_type == PositionTypeEnum.LONG_SHORT_COMBINED
 
         for index, row in df.iterrows():
-            current_price = row['Close']
+            current_price = row[SourceType.CLOSE.value]
 
-            if bought and current_price >= buying_price + value_threshold:
-                df.at[index, 'SELL'] = True
-                df.at[index, 'SELL_Signals'] = f'TakeProfit({self.percentage})'
-                bought = False
+            # Reset bought/sold flags if opposing signals occur (e.g., SELL after BUY)
+            if bought and row['SELL']:
+                if position_type == PositionTypeEnum.LONG_SHORT_COMBINED:
+                    if not row['BUY']:
+                        bought = False
+                        sold = False
+                else:
+                    bought = False
+                    continue
 
-            if row['BUY']:
+            elif sold and row['BUY']:
+                if position_type == PositionTypeEnum.LONG_SHORT_COMBINED:
+                    if not row['SELL']:
+                        sold = False
+                        bought = False
+                else:
+                    sold = False
+                    continue
+
+            # Handle LONG positions: Trigger 'SELL' when take-profit is reached
+            if has_long and not bought and not sold and row['BUY'] and not row['SELL']:
                 buying_price = current_price
                 value_threshold = (buying_price * self.percentage) / 100
                 bought = True
+                continue
+
+            # Handle SHORT positions: Trigger 'BUY' when take-profit is reached
+            elif has_short and not bought and not sold and row['SELL'] and not row['BUY']:
+                selling_price = current_price
+                value_threshold = (selling_price * self.percentage) / 100
+                sold = True
+                continue
+
+            # Check if the take-profit threshold is reached for LONG positions
+            if has_long and bought and current_price >= buying_price + value_threshold:
+                df.at[index, 'SELL'] = True
+                df.at[index, 'BUY'] = False  # Prioritize Selling
+                df.at[index, 'SELL_Signals'] = f'TakeProfit({self.percentage})'
+                bought = False
+
+            # Check if the take-profit threshold is reached for SHORT positions
+            if has_short and sold and current_price <= selling_price - value_threshold:
+                df.at[index, 'BUY'] = True
+                df.at[index, 'SELL'] = False  # Prioritize Buying
+                df.at[index, 'BUY_Signals'] = f'TakeProfit({self.percentage})'
+                sold = False
