6d1a5a8e378ab5913259d521ae5e8fa927a58122
Daniel Soltys <danielsytlos@gmail.com>
2024-09-20 22:39:03 +0200

    Added fibonacci levels enum

---
+++ trading_strategy_tester/enums/fibonacci_levels_enum.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class FibonacciLevels(Enum):
+    LEVEL_0 = '0'
+    LEVEL_23_6 = '23.6'
+    LEVEL_38_2 = '38.2'
+    LEVEL_50 = '50'
+    LEVEL_61_8 = '61.8'
+    LEVEL_100 = '100'
