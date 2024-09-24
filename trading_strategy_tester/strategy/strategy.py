89e45ca2e3a1772143402865033cd66c7b95de23
Daniel Soltys <danielsytlos@gmail.com>
2024-09-24 21:58:09 +0200

    Added documentary comments and statistics to strategy

--- trading_strategy_tester/strategy/strategy.py
+++ trading_strategy_tester/strategy/strategy.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+
+import pandas as pd
+
 from trading_strategy_tester.conditions.condition import Condition
 from trading_strategy_tester.conditions.stoploss_takeprofit.stop_loss import StopLoss
 from trading_strategy_tester.conditions.stoploss_takeprofit.take_profit import TakeProfit
@@ -7,6 +10,7 @@ from trading_strategy_tester.download.download_module import DownloadModule
 from trading_strategy_tester.enums.interval_enum import Interval
 from trading_strategy_tester.enums.period_enum import Period
 from trading_strategy_tester.enums.position_type_enum import PositionTypeEnum
+from trading_strategy_tester.statistics.statistics import get_strategy_stats
 from trading_strategy_tester.trade.trade import create_all_trades
 from trading_strategy_tester.trade.trade_commissions.money_commissions import MoneyCommissions
 from trading_strategy_tester.trade.trade_commissions.trade_commissions import TradeCommissions
@@ -14,8 +18,25 @@ from trading_strategy_tester.utils.validations import get_position_type_from_enu
 
 
 class Strategy:
+    """
+    A trading strategy that defines conditions for buying and selling
+    a financial instrument, with optional stop loss and take profit features.
+
+    :param ticker: The financial instrument to trade.
+    :param position_type: The type of position to take (e.g., long or short).
+    :param buy_condition: The condition that must be met to execute a buy.
+    :param sell_condition: The condition that must be met to execute a sell.
+    :param stop_loss: Optional stop loss condition.
+    :param take_profit: Optional take profit condition.
+    :param start_date: The start date for backtesting (default is 2024-01-01).
+    :param end_date: The end date for backtesting (default is today).
+    :param interval: The time interval for the trading data (default is daily).
+    :param period: The period for which to evaluate the conditions (default is not passed).
+    :param trade_commissions: The commissions associated with trades (default is zero commission).
+    """
+
     def __init__(self,
-                 ticker:str,
+                 ticker: str,
                  position_type: PositionTypeEnum,
                  buy_condition: Condition,
                  sell_condition: Condition,
@@ -44,7 +65,15 @@ class Strategy:
         self.trades = list()
         self.stats = dict()
 
-    def execute(self):
+    def execute(self) -> pd.DataFrame:
+        """
+        Executes the trading strategy by downloading data, evaluating
+        conditions, setting stop losses and take profits, and generating
+        trade statistics.
+
+        :return: A DataFrame containing the evaluated conditions for buying and selling.
+        :rtype: pd.DataFrame
+        """
         downloader = DownloadModule(self.start_date, self.end_date, self.interval, self.period)
         df = downloader.download_ticker(self.ticker)
 
@@ -62,7 +91,7 @@ class Strategy:
         if self.stop_loss is not None:
             self.stop_loss.set_stop_loss(evaluated_conditions_df, self.position_type_enum)
 
-        # Clean the BUY and SELL columns based of the position type
+        # Clean the BUY and SELL columns based on the position type
         self.position_type.clean_buy_sell_columns(evaluated_conditions_df)
 
         # Create Graphs
@@ -72,19 +101,36 @@ class Strategy:
         self.trades = create_all_trades(df, self.trade_commissions)
 
         # Create stats of the strategy
-        #self.stats =
+        self.stats = get_strategy_stats(self.trades, evaluated_conditions_df, None)
 
         # Delete temp downloaded files
         downloader.delete_temp_files()
 
         return evaluated_conditions_df
 
-    def get_trades(self):
+    def get_trades(self) -> list:
+        """
+        Returns the list of trades executed by the strategy.
+
+        :return: A list of trades.
+        :rtype: list
+        """
         return self.trades
 
     def get_graphs(self) -> dict:
+        """
+        Returns the generated graphs based on trading conditions.
+
+        :return: A dictionary containing the generated graphs.
+        :rtype: dict
+        """
         return self.graphs
 
-    def get_statistics(self):
-        # TODO
-        pass
+    def get_statistics(self) -> dict:
+        """
+        Returns the statistics of the trading strategy.
+
+        :return: A dictionary containing the strategy statistics.
+        :rtype: dict
+        """
+        return self.stats
