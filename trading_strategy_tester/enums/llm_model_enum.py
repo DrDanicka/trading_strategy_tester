b469e46b5468407dc2687d5e4adaa78356044b50
Daniel Soltys <danielsytlos@gmail.com>
2025-03-21 23:07:14 +0100

    Added dict for backward mapping

--- trading_strategy_tester/enums/llm_model_enum.py
+++ trading_strategy_tester/enums/llm_model_enum.py
@@ -4,3 +4,9 @@ class LLMModel(Enum):
     LLAMA_1B = 'llama3_2-1B_tst_ft'
     QWEN_0_5B = 'qwen2_5-0_5B_tst_ft'
     CHAT_GPT_API = 'chat-gpt-api'
+
+llm_model_dict = {
+    'llama3_2-1B_tst_ft': LLMModel.LLAMA_1B,
+    'qwen2_5-0_5B_tst_ft': LLMModel.QWEN_0_5B,
+    'chat-gpt-api': LLMModel.CHAT_GPT_API
+}
