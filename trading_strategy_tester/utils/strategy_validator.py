05be175992ea53716260d0629c6e387f96c3bc38
Daniel Soltys <danielsytlos@gmail.com>
2025-03-31 22:50:15 +0200

    Added date validation

--- trading_strategy_tester/utils/strategy_validator.py
+++ trading_strategy_tester/utils/strategy_validator.py
@@ -1,6 +1,6 @@
 import ast
 import pandas as pd
-
+from datetime import datetime
 from trading_strategy_tester.enums.position_type_enum import PositionTypeEnum
 
 implemented_conditions = pd.DataFrame(
@@ -141,9 +141,41 @@ def validate_position_type(position_type, changes: dict, logs: bool) -> (bool, s
 
 
 def validate_condition(condition, changes: dict, logs: bool, buy: bool) -> (bool, str, dict):
-    print(ast.dump(condition))
+    # print(ast.dump(condition))
     return True, None, changes
 
+def validate_date(date, changes: dict, logs: bool, start: bool) -> (bool, str, dict):
+    if start:
+        default_date = datetime(2024, 1, 1)
+    else:
+        default_date = datetime.today()
+    message = f"Date argument should be of type datetime. Using default date '{default_date.strftime('%Y-%m-%d')}'."
+    not_valid = False
+
+    try:
+        str_datetime = date.func.id
+        year, month, day = [i.value for i in date.args]
+    except Exception:
+        not_valid = True
+
+    if str_datetime != 'datetime':
+        not_valid = True
+
+    if not not_valid:
+        passed_date = datetime(year, month, day)
+        if passed_date > datetime.today():
+            message = f"Date argument should be a date in the past. Using default date '{default_date.strftime('%Y-%m-%d')}'."
+            not_valid = True
+
+    if not_valid:
+        if logs:
+            print(message)
+
+        changes['start_date' if start else 'end_date'] = message
+
+        return False, default_date, changes
+
+    return True, None, changes
 
 def validate_strategy_string(strategy_str: str, logs: bool = False) -> (bool, str):
     changes = dict()
@@ -209,6 +241,22 @@ def validate_strategy_string(strategy_str: str, logs: bool = False) -> (bool, st
                 # If the sell condition is not valid, set it to the default sell condition
                 if not validation_result:
                     kwarg.value = ast.Constant(value=condition)
+
+            if kwarg.arg in ['start_date', 'end_date']:
+                validation_result, date, changes = validate_date(kwarg.value, changes, logs, start=True if kwarg.arg == 'start_date' else False)
+
+                # If the date is not valid, set it to the default date
+                if not validation_result:
+                    kwarg.value = ast.Call(
+                        func=ast.Name(id='datetime', ctx=ast.Load()),
+                        args=[
+                            ast.Constant(value=date.year),
+                            ast.Constant(value=date.month),
+                            ast.Constant(value=date.day),
+                        ],
+                        keywords=[]
+                    )
+
 
 
 
