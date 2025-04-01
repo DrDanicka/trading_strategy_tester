203aa3218d0fbc45ea878877b5aaa87483d7379a
Daniel Soltys <danielsytlos@gmail.com>
2025-04-01 20:30:34 +0200

    Added stop_loss and take_profit validations

--- trading_strategy_tester/utils/strategy_validator.py
+++ trading_strategy_tester/utils/strategy_validator.py
@@ -177,6 +177,97 @@ def validate_date(date, changes: dict, logs: bool, start: bool) -> (bool, str, d
 
     return True, None, changes
 
+def validate_stop_loss(stop_loss, changes: dict, logs: bool) -> (bool, str, dict):
+    default_stop_loss = None
+    message = f"stop_loss argument should be of type StopLoss. Defaulting to no stop loss."
+    not_valid = False
+
+    try:
+        stop_loss_type = stop_loss.func.id
+
+        if stop_loss_type == 'StopLoss':
+            param1 = stop_loss.keywords[0]
+            param2 = stop_loss.keywords[1]
+
+            if param1.arg == 'percentage':
+                stop_loss_percentage = param1.value
+            elif param2.arg == 'percentage':
+                stop_loss_percentage = param2.value
+            else:
+                message = f"stop_loss argument percentage is missing. Defaulting to no stop loss."
+                raise Exception("percentage not found")
+
+            if param1.arg == 'stop_loss_type':
+                stop_loss_type = param1.value
+            elif param2.arg == 'stop_loss_type':
+                stop_loss_type = param2.value
+            else:
+                message = f"stop_loss argument stop_loss_type is missing. Defaulting to no stop loss."
+                raise Exception("stop_loss_type not found")
+
+            if stop_loss_type.value.id != 'StopLossType':
+                message = f"stop_loss argument stop_loss_type should be of type StopLossType. Defaulting to no stop loss."
+                raise Exception("stop_loss_type not found")
+
+            if stop_loss_type.attr not in ['NORMAL', 'TRAILING']:
+                message = f"Valid stop_loss_types are: 'NORMAL', 'TRAILING'. Defaulting to no stop loss."
+                raise Exception("stop_loss_type not found")
+
+            # Check stop_loss_percentage.value is float
+            if not isinstance(stop_loss_percentage.value, (int, float)):
+                message = f"stop_loss argument percentage should be a number. Defaulting to no stop loss."
+                raise Exception("percentage not found")
+        else:
+            not_valid = True
+
+    except Exception:
+        not_valid = True
+
+    if not_valid:
+        if logs:
+            print(message)
+
+        changes['stop_loss'] = message
+
+        return False, default_stop_loss, changes
+
+    return True, None, changes
+
+
+def validate_take_profit(take_profit, changes: dict, logs: bool) -> (bool, str, dict):
+    default_take_profit = None
+    message = f"take_profit argument should be of type TakeProfit. Defaulting to no take profit."
+    not_valid = False
+
+    try:
+        take_profit_type = take_profit.func.id
+
+        if take_profit_type == 'TakeProfit':
+            take_profit_percentage = take_profit.keywords[0]
+
+            if take_profit_percentage.arg != 'percentage':
+                message = f"take_profit argument percentage is missing. Defaulting to no take profit."
+                raise Exception("percentage not found")
+
+            if not isinstance(take_profit_percentage.value.value, (int, float)):
+                message = f"take_profit argument percentage should be a number. Defaulting to no take profit."
+                raise Exception("percentage not found")
+
+        else:
+            not_valid = True
+    except Exception:
+        not_valid = True
+
+    if not_valid:
+        if logs:
+            print(message)
+
+        changes['take_profit'] = message
+
+        return False, default_take_profit, changes
+
+    return True, None, changes
+
 def validate_strategy_string(strategy_str: str, logs: bool = False) -> (bool, str):
     changes = dict()
 
@@ -257,8 +348,26 @@ def validate_strategy_string(strategy_str: str, logs: bool = False) -> (bool, st
                         keywords=[]
                     )
 
+            if kwarg.arg == 'stop_loss':
+                # If it's not None
+                if type(kwarg.value) is not ast.Constant:
+                    validation_result, stop_loss, changes = validate_stop_loss(kwarg.value, changes, logs)
 
+                    # If the stop loss is not valid, set it to the default stop loss (None)
+                    if not validation_result:
+                        kwarg.value = ast.Constant(value=stop_loss)
 
+            if kwarg.arg == 'take_profit':
+                # If it's not None
+                if type(kwarg.value) is not ast.Constant:
+                    validation_result, take_profit, changes = validate_take_profit(kwarg.value, changes, logs)
+
+                    # If the take profit is not valid, set it to the default take profit (None)
+                    if not validation_result:
+                        kwarg.value = ast.Constant(value=take_profit)
+
+            # if kwarg.arg == 'interval':
+            #     val
 
 
     except Exception as e:
