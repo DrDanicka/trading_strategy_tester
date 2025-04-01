7596da1dfd0588756fcd98fc400752fc560f85b4
Daniel Soltys <danielsytlos@gmail.com>
2025-04-01 20:41:30 +0200

    Added interval validation

--- trading_strategy_tester/utils/strategy_validator.py
+++ trading_strategy_tester/utils/strategy_validator.py
@@ -1,6 +1,8 @@
 import ast
 import pandas as pd
 from datetime import datetime
+
+from trading_strategy_tester.enums.interval_enum import Interval
 from trading_strategy_tester.enums.position_type_enum import PositionTypeEnum
 
 implemented_conditions = pd.DataFrame(
@@ -268,6 +270,41 @@ def validate_take_profit(take_profit, changes: dict, logs: bool) -> (bool, str, 
 
     return True, None, changes
 
+
+def validate_interval(interval, changes: dict, logs: bool) -> (bool, str, dict):
+    default_interval = Interval.ONE_DAY
+    message = f"interval argument should be of type Interval. Defaulting to {default_interval}."
+    not_valid = False
+
+    print(ast.dump(interval))
+
+    try:
+        interval_enum = interval.value.id
+        interval_attr = interval.attr
+
+        if interval_enum != 'Interval':
+            raise Exception("Invalid interval enum")
+
+        valid_intervals = ['ONE_DAY', 'FIVE_DAYS', 'ONE_WEEK', 'ONE_MONTH', 'THREE_MONTHS']
+
+        if interval_attr not in valid_intervals:
+            message = f"Valid intervals are: {', '.join(valid_intervals)}. Defaulting to {default_interval}."
+            raise Exception("Invalid interval attr")
+
+    except Exception:
+        not_valid = True
+
+    if not_valid:
+        if logs:
+            print(message)
+
+        changes['interval'] = message
+
+        return False, default_interval, changes
+
+    return True, None, changes
+
+
 def validate_strategy_string(strategy_str: str, logs: bool = False) -> (bool, str):
     changes = dict()
 
@@ -366,8 +403,8 @@ def validate_strategy_string(strategy_str: str, logs: bool = False) -> (bool, st
                     if not validation_result:
                         kwarg.value = ast.Constant(value=take_profit)
 
-            # if kwarg.arg == 'interval':
-            #     val
+            if kwarg.arg == 'interval':
+                validation_result, interval, changes = validate_interval(kwarg.value, changes, logs)
 
 
     except Exception as e:
@@ -378,4 +415,4 @@ def validate_strategy_string(strategy_str: str, logs: bool = False) -> (bool, st
     return True, 'xd'
 
 
-validate_strategy_string("Strategy(ticker='TSLA', position_type=PositionTypeEnum.LONG, buy_condition=OR( CrossOverCondition(first_series=RSI('TSLA'), second_series=CONST(30)), LessThanCondition(first_series=EMA('TSLA'), second_series=CLOSE('TSLA'))), sell_condition=OR( CrossOverCondition(first_series=CONST(70), second_series=RSI('TSLA')), IntraIntervalChangeOfXPercentCondition(series=CLOSE('TSLA'), percent=5)), stop_loss=StopLoss(percentage=5, stop_loss_type=StopLossType.NORMAL), take_profit=TakeProfit(percentage=10), start_date=datetime(2020, 1, 1), end_date=datetime(2024, 1, 1), initial_capital=1000)")
+validate_strategy_string("Strategy(interval=Interval.ONE_WEEK, ticker='TSLA', position_type=PositionTypeEnum.LONG, buy_condition=OR( CrossOverCondition(first_series=RSI('TSLA'), second_series=CONST(30)), LessThanCondition(first_series=EMA('TSLA'), second_series=CLOSE('TSLA'))), sell_condition=OR( CrossOverCondition(first_series=CONST(70), second_series=RSI('TSLA')), IntraIntervalChangeOfXPercentCondition(series=CLOSE('TSLA'), percent=5)), stop_loss=StopLoss(percentage=5, stop_loss_type=StopLossType.NORMAL), take_profit=TakeProfit(percentage=10), start_date=datetime(2020, 1, 1), end_date=datetime(2024, 1, 1), initial_capital=1000)")
