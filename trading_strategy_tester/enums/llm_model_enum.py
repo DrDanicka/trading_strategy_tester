ff640e469fb8a0bfcd3c2135e1ae0248e289d7e8
Daniel Soltys <danielsytlos@gmail.com>
2025-04-29 23:05:41 +0200

    Added all possibilities to llm enums

--- trading_strategy_tester/enums/llm_model_enum.py
+++ trading_strategy_tester/enums/llm_model_enum.py
@@ -1,16 +1,16 @@
 from enum import Enum
 
 class LLMModel(Enum):
-    LLAMA_3B = 'llama3_2-3B_tst_ft'
-    LLAMA_1B = 'llama3_2-1B_tst_ft'
-    QWEN_0_5B = 'qwen2_5-0_5B_tst_ft'
+    LLAMA_ALL = 'llama3-2-3B_tst_ft-all'
+    LLAMA_FIELDS = 'llama3-2-1B_tst_ft-'
+    LLAMA_ALL_RAG = 'llama3-2-all-rag'
+    LLAMA_FIELDS_RAG = 'llama3-2-fields-rag'
     STRATEGY_OBJECT = 'strategy_object'
-    CHAT_GPT_API = 'chat-gpt-api'
 
 llm_model_dict = {
-    'llama3_2-3B_tst_ft': LLMModel.LLAMA_3B,
-    'llama3_2-1B_tst_ft': LLMModel.LLAMA_1B,
-    'qwen2_5-0_5B_tst_ft': LLMModel.QWEN_0_5B,
+    'llama3-2-3B_tst_ft-all': LLMModel.LLAMA_ALL,
+    'llama3-2-1B_tst_ft-': LLMModel.LLAMA_FIELDS,
     'strategy_object': LLMModel.STRATEGY_OBJECT,
-    'chat-gpt-api': LLMModel.CHAT_GPT_API
+    'llama3-2-all-rag': LLMModel.LLAMA_ALL_RAG,
+    'llama3-2-fields-rag': LLMModel.LLAMA_FIELDS_RAG,
 }
