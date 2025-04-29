fb7b1eb45ee9166e4c6ebb54136c7ff248f929be
Daniel Soltys <danielsytlos@gmail.com>
2025-04-29 23:04:27 +0200

    Modified prompts for RAG

--- trading_strategy_tester/llm_communication/rag/get_rag_response.py
+++ trading_strategy_tester/llm_communication/rag/get_rag_response.py
@@ -1,4 +1,4 @@
-from rag.extract_parameter import extract_parameter
+from trading_strategy_tester.llm_communication.rag.extract_parameter import extract_parameter
 
 def get_rag_response(model, prompt):
     model_for = model.split('-')[-2]
